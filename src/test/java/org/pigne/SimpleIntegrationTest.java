02f5a550a5da2a95941750b0820b36b9c7a7686c
Yoann Pigné <yoann.pigne@gmail.com>
2014-11-30 21:18:57 +0100

    Integration tests with Selenium.
    Add 1 integration test in the test folder. It uses the Selenium web
    browser automator.
    We modify the maven-tomcat config in order to start and stop only for
    the integration tests.

---
+++ src/test/java/org/pigne/SimpleIntegrationTest.java
@@ -0,0 +1,92 @@
+package org.pigne;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.NoSuchElementException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SimpleIntegrationTest {
+	private String baseUrl;
+	private WebDriver driver;
+	private ScreenshotHelper screenshotHelper;
+
+	@Before
+	public void openBrowser() {
+		baseUrl = "http://localhost:9090/CountDownWebApp/countdown";
+		driver = new HtmlUnitDriver();
+		//driver = new FirefoxDriver();
+		// screenshotHelper = new ScreenshotHelper();
+	}
+
+	@Test
+	public void testPage() {
+
+		driver.get(baseUrl);
+		//System.out.println(driver.getPageSource());
+
+		try {
+			// Find the title of the page
+			WebElement element = driver.findElement(By.tagName("title"));
+			assertNotNull(element);
+			assertEquals("CountDown", element.getText().trim());
+			
+			// Find the footer that should contain "Nobody"
+			element = driver.findElement(By.tagName("footer"));
+			assertNotNull(element);
+			assertEquals("Nobody", element.getText().trim());
+
+		} catch (NoSuchElementException ex) {
+			fail("Startup of context failed. See console output for more information, : "
+					+ ex.getMessage());
+		}
+	}
+
+	@Test
+	public void testAuthorParameter() {
+
+		driver.get(baseUrl + "?author=Me");
+		//System.out.println(driver.getPageSource());
+
+		try {
+
+			// Find the footer that should contain "Me"
+			WebElement element = driver.findElement(By.tagName("footer"));
+			assertNotNull(element);
+			assertEquals("Me", element.getText().trim());
+
+		} catch (NoSuchElementException ex) {
+			fail("Startup of context failed. See console output for more information, : "
+					+ ex.getMessage());
+		}
+	}
+
+	@After
+	public void saveScreenshotAndCloseBrowser() throws IOException {
+		// screenshotHelper.saveScreenshot("screenshot.png");
+		driver.quit();
+	}
+
+	private class ScreenshotHelper {
+
+		public void saveScreenshot(String screenshotFileName)
+				throws IOException {
+			File screenshot = ((TakesScreenshot) driver)
+					.getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(screenshot, new File(screenshotFileName));
+		}
+	}
+
+}
